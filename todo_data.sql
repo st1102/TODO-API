935cbab6dcccc85294c6b82b8c29d2b976c6eddc
vagrant <vagrant@10.0.2.15>
2017-09-10 12:43:41 +0900

    change table name

--- todo_data.sql
+++ todo_data.sql
@@ -1,6 +1,6 @@
 create database todo_api
 
-create table todo_lists (
+create table todos (
   id int not null auto_increment primary key,
   title varchar(255),
   deadline datetime,
@@ -8,15 +8,15 @@ create table todo_lists (
 );
 
 #仮データ挿入
-insert into todo_lists (title, deadline) values(
+insert into todos (title, deadline) values(
   "title1",
   "2017-09-15 00:00:00"
 );
-insert into todo_lists (title, deadline) values(
+insert into todos (title, deadline) values(
   "title2",
   "2017-09-15 12:00:00"
 );
-insert into todo_lists (title, deadline) values(
+insert into todos (title, deadline) values(
   "title1",
   "2017-09-15 23:59:59"
 );
